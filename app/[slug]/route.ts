c7fc85f449ecd9020cb10d521d8bb630da7ffd7d
Ahlström Kalle <ahlstrom.kalle@outlook.com>
2023-08-18 05:57:38 +0300

    cache path-redirects

--- app/[slug]/route.ts
+++ app/[slug]/route.ts
@@ -1,16 +1,28 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getDB } from "../../lib/db";
+import { unstable_cache } from "next/cache";
 
 export const runtime = "edge";
 
 export async function GET(request: NextRequest) {
   const { pathname } = request.nextUrl;
-  const db = getDB();
-  const doc = await db
-    .selectFrom("shortened_links")
-    .select("url")
-    .where("shorthand", "=", pathname.slice(1))
-    .executeTakeFirst();
+  const shorthand = pathname.slice(1);
+  const doc = await unstable_cache(
+    async () => {
+      console.log("fetching path", shorthand);
+      const db = getDB();
+      const doc = await db
+        .selectFrom("shortened_links")
+        .select(["url"])
+        .where("shorthand", "=", shorthand)
+        .executeTakeFirst();
+
+      return doc;
+    },
+    ["getRedirectforPath", pathname],
+    { tags: ["getRedirectforPath" + pathname], revalidate: 3600 }
+  )();
+
   if (!doc) {
     return new NextResponse("Not found", { status: 404 });
   }
