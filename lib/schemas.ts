8783bbe2fd9bb3a5c5073e31a59434db10ac3ed5
Ahlström Kalle <ahlstrom.kalle@outlook.com>
2023-09-10 01:24:49 +0300

    increase url max-length

--- lib/schemas.ts
+++ lib/schemas.ts
@@ -4,7 +4,7 @@ export const formSchema = z.object({
   url: z
     .string()
     .url("Please enter a valid URL")
-    .max(500, "URL must be less than 500 characters"),
+    .max(2000, "URL must be shorter than 2000 characters"),
   shorthand: z
     .string()
     .max(30, "shorthand must be less than 30 characters")
